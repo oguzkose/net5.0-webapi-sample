c8fae32698f969ca67949a6df93308e4f83f6fc4
oguzkose <oguzkose2019@outlook.com>
2022-02-16 18:59:57 +0300

    Http Put - Delete - Post Endpoints added

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -50,5 +50,39 @@ namespace WebApi.Controllers
             var book = BookList.Where(x => x.Id == id).SingleOrDefault();
             return book;
         }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book is not null)
+                return BadRequest();
+
+            BookList.Add(newBook);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+                return BadRequest();
+
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+                return BadRequest();
+            BookList.Remove(book);
+            return Ok();
+        }
     }
 }
