34b198892775337702034fcf095a82ef991c9a29
oguzkose <oguzkose2019@outlook.com>
2022-03-05 18:31:30 +0300

    new Author datas added

--- WebApi/DbOperations/DataGenerator.cs
+++ WebApi/DbOperations/DataGenerator.cs
@@ -59,6 +59,28 @@ namespace WebApi.DBOperations
                         Name = "History",
                     }
                 );
+
+                context.Authors.AddRange(
+                    new Author
+                    {
+                        Name = "Mustafa Kemal",
+                        Surname = "Atatürk",
+                        DateOfBirth = new DateTime(1881, 05, 19)
+                    },
+                    new Author
+                    {
+                        Name = "Eckhart",
+                        Surname = "Tolle",
+                        DateOfBirth = new DateTime(1948, 12, 10)
+                    },
+                    new Author
+                    {
+                        Name = "Frank",
+                        Surname = "Herbert",
+                        DateOfBirth = new DateTime(1920, 10, 08)
+                    }
+                );
+                
                 context.SaveChanges();
             }
 
