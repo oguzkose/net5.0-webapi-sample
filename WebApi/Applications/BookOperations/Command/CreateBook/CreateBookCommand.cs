9f2616205edb121df222402ab399e70c21ed9c10
oguzkose <oguzkose2019@outlook.com>
2022-03-08 12:31:39 +0300

    Author Included to CreateBookCommand

--- WebApi/Applications/BookOperations/Command/CreateBook/CreateBookCommand.cs
+++ WebApi/Applications/BookOperations/Command/CreateBook/CreateBookCommand.cs
@@ -33,6 +33,7 @@ namespace WebApi.Applications.BookOperations.Command.CreateBook
     {
 
         public string Title { get; set; }
+        public int AuthorId { get; set; }
         public int GenreId { get; set; }
         public int PageCount { get; set; }
         public DateTime PublishDate { get; set; }
