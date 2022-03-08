f3eeb1d23058318c265fbc7aa7bb4378fbcbaaff
oguzkose <oguzkose2019@outlook.com>
2022-03-08 12:23:13 +0300

    AuthorId added to Books

--- WebApi/DbOperations/DataGenerator.cs
+++ WebApi/DbOperations/DataGenerator.cs
@@ -23,6 +23,7 @@ namespace WebApi.DBOperations
                     new Book
                     {
                         Title = "Nutuk",
+                        AuthorId = 1,
                         GenreId = 3,
                         PageCount = 450,
                         PublishDate = new DateTime(1937, 05, 13)
@@ -30,6 +31,7 @@ namespace WebApi.DBOperations
                     new Book
                     {
                         Title = "Şimdinin Gücü",
+                        AuthorId = 2,
                         GenreId = 1,
                         PageCount = 350,
                         PublishDate = new DateTime(2018, 12, 01)
@@ -38,6 +40,7 @@ namespace WebApi.DBOperations
                     new Book
                     {
                         Title = "Dune",
+                        AuthorId = 3,
                         GenreId = 2,
                         PageCount = 520,
                         PublishDate = new DateTime(1989, 02, 15)
@@ -80,7 +83,7 @@ namespace WebApi.DBOperations
                         DateOfBirth = new DateTime(1920, 10, 08)
                     }
                 );
-                
+
                 context.SaveChanges();
             }
 
