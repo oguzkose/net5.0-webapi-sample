914343d33dcd12fc67e5f80702bfff733114079e
oguzkose <oguzkose2019@outlook.com>
2022-03-08 12:30:54 +0300

    CreateBookCommandValidator updated

--- WebApi/Applications/BookOperations/Command/CreateBook/CreateBookCommandValidator.cs
+++ WebApi/Applications/BookOperations/Command/CreateBook/CreateBookCommandValidator.cs
@@ -8,6 +8,7 @@ namespace WebApi.Applications.BookOperations.Command.CreateBook
         public CreateBookCommandValidator()
         {
             RuleFor(command => command.Model.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.AuthorId).GreaterThan(0);
             RuleFor(command => command.Model.PageCount).GreaterThan(0);
             RuleFor(command => command.Model.PublishDate.Date).NotEmpty().LessThan(DateTime.Now.Date);
             RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(1).MaximumLength(100);
