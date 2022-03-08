61554747164d5b3fbfc0ab614e52d3ddd7a6af74
oguzkose <oguzkose2019@outlook.com>
2022-03-08 12:22:03 +0300

    DeleteAuthorCommand added

--- WebApi/Applications/AuthorOperations/Command/DeleteAuthor/DeleteAuthorCommand.cs
+++ WebApi/Applications/AuthorOperations/Command/DeleteAuthor/DeleteAuthorCommand.cs
@@ -16,14 +16,18 @@ namespace WebApi.Applications.AuthorOperations.Command.DeleteAuthor
         public int AuthorId { get; set; }
         public void Handle()
         {
+            
             var author = _context.Authors.SingleOrDefault(x => x.Id == AuthorId);
             if (author is null)
                 throw new InvalidOperationException(AuthorId + " numaralı silinecek yazar bulunamadı");
+
+            var books = _context.Books.Where(x => x.AuthorId == AuthorId).ToList();
+            _context.Books.RemoveRange(books);
 
             _context.Authors.Remove(author);
             _context.SaveChanges();
         }
 
     }
-    
+
 }
