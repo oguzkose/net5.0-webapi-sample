0e8473476de0c507457ae03d9f4fa71b77c30d21
oguzkose <oguzkose2019@outlook.com>
2022-03-10 13:26:56 +0300

    CreateAuthorCommandTest added

---
+++ Tests/WebApi.UnitTest/Applications/AuthorOperations/Commands/CreateAuthor/CreateAuthorCommandTest.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using FluentAssertions;
+using Tests.WebApi.UnitTest.TestSetup;
+using WebApi.Applications.AuthorOperations.Command.CreateAuthor;
+using WebApi.DBOperations;
+using WebApi.Entities;
+using Xunit;
+
+namespace Tests.WebApi.UnitTest.Applications.AuthorOperations.Commands.CreateAuthor
+{
+    public class CreateAuthorCommandTest : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public CreateAuthorCommandTest(CommonTestFixture fixture)
+        {
+            _context = fixture.Context;
+            _mapper = fixture.Mapper;
+        }
+        [Fact]
+        public void WhenAlreadyExistAuthorNameAndSurnameAreGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            var author = new Author
+            {
+                Name = "Nikos",
+                Surname = "Kazancakis",
+                DateOfBirth = new DateTime(1900, 12, 10)
+            };
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+
+            CreateAuthorCommand command = new CreateAuthorCommand(_context, _mapper);
+            command.Model = new CreateAuthorModel()
+            {
+                Name = author.Name,
+                Surname = author.Surname
+            };
+
+            FluentActions.Invoking(
+                () => command.Handle()
+                    )
+                    .Should().Throw<InvalidOperationException>()
+                    .And.Message.Should().Be("Yazar zaten mevcut.");
+
+        }
+
+
+        [Fact]
+        public void WhenValidInputsAreGiven_Author_ShouldBeCreated()
+        {
+            //Arrange
+            CreateAuthorCommand command = new CreateAuthorCommand(_context, _mapper);
+            CreateAuthorModel model = new CreateAuthorModel()
+            {
+                Name = "Ursula",
+                Surname = "Le Guin",
+                DateOfBirth = new DateTime(1919, 05, 21)
+            };
+            command.Model = model;
+
+            //Act
+            FluentActions.Invoking(
+                () => command.Handle()
+            ).Invoke();
+
+            //Arrange
+            var author = _context.Authors.First(x => x.Name == model.Name && x.Surname == model.Surname);
+
+            author.Should().NotBeNull();
+            author.Name.Should().Be(model.Name);
+            author.DateOfBirth.Should().Be(model.DateOfBirth);
+
+        }
+
+    }
+}
