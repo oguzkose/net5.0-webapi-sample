a38557e0126653a715ffccc1b1c2d9529764c01f
oguzkose <oguzkose2019@outlook.com>
2022-03-12 16:46:33 +0300

    GetAuthorDetailQueryValidator added

---
+++ WebApi/Applications/AuthorOperations/Query/GetAuthorDetail/GetAuthorDetailQueryValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace WebApi.Applications.AuthorOperations.Query.GetAuthorDetail
+{
+    public class GetAuthorDetailQueryValidator : AbstractValidator<GetAuthorDetailQuery>
+    {
+        public GetAuthorDetailQueryValidator()
+        {
+            RuleFor(query => query.AuthorId).GreaterThan(0);
+        }
+    }
+}
