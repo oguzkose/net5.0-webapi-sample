473ff4b735037e06973681c37ba6c3f15aa59e53
oguzkose <oguzkose2019@outlook.com>
2022-03-17 16:31:21 +0300

    GetBookDetailQueryValidatorTest updated

--- Tests/WebApi.UnitTest/Applications/BookOperations/Query/GetBookDetail/GetBookDetailQueryValidatorTest.cs
+++ Tests/WebApi.UnitTest/Applications/BookOperations/Query/GetBookDetail/GetBookDetailQueryValidatorTest.cs
@@ -29,11 +29,12 @@ namespace Tests.WebApi.UnitTest.Applications.BookOperations.Query.GetBookDetail
         public void WhenValidBookIdIsGiven_Validator_ShouldNotBeReturnError(int bookId)
         {
             GetBookDetailQuery query = new GetBookDetailQuery(null, null);
+            query.BookId = bookId;
 
             GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
             var result = validator.Validate(query);
 
-            result.Errors.Count.Should().Equals(0);
+            result.Errors.Count.Should().BeLessThan(1);
         }
         #endregion
     }
