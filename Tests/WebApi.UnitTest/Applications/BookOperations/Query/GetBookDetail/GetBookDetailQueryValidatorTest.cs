69183780798d85884ad812186a11831d9be0d149
oguzkose <oguzkose2019@outlook.com>
2022-03-17 14:50:54 +0300

    GetBookDetailQueryValidatorTest added

---
+++ Tests/WebApi.UnitTest/Applications/BookOperations/Query/GetBookDetail/GetBookDetailQueryValidatorTest.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using WebApi.Applications.BookOperations.Query.GetBookDetail;
+using Xunit;
+
+namespace Tests.WebApi.UnitTest.Applications.BookOperations.Query.GetBookDetail
+{
+    public class GetBookDetailQueryValidatorTest
+    {
+        #region WhenInvalidBookIdIsGiven_Validator_ShouldBeReturnError()
+        [Theory]
+        [InlineData(-5)]
+        [InlineData(0)]
+        public void WhenInvalidBookIdIsGiven_Validator_ShouldBeReturnError(int bookId)
+        {
+            GetBookDetailQuery query = new GetBookDetailQuery(null, null);
+
+            GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+        #endregion
+
+        #region WhenValidBookIdIsGiven_Validator_ShouldNotBeReturnError()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void WhenValidBookIdIsGiven_Validator_ShouldNotBeReturnError(int bookId)
+        {
+            GetBookDetailQuery query = new GetBookDetailQuery(null, null);
+
+            GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().Equals(0);
+        }
+        #endregion
+    }
+}
